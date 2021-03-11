f4f9a3843ff9323abb790c457aa3f97750f53a5c
Tri <triphan921@gmail.com>
2021-03-11 14:58:59 -0800

    completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 class Formatter {
-  //add static methods here
+  static capitalize(str) {
+    return str[0].toUpperCase() + str.slice(1);
+  }
+
+  static sanitize(str) {
+    return string.replace( /[^A-Za-z0-9 '-]/g, '' );
+  }
+
+  static titleize(str) {
+    let arr = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    let words = str.split(' ')
+    return words.map(word=> arr.includes(word) && word !== words[0]? word : this.capitalize(word)).join(' ');
+  }
 }
